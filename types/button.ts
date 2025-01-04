4dd40031c39df36304177ea76ccb8997c51a5b4e
Noey Ignacio <ignacio.francis@aelluminate.com>
2025-01-04 23:43:29 +0800

    feat: add type definitions for Button and Sheet components
    - Introduces type definitions for Button and Sheet components to enhance type safety and code clarity.
    - Defines props interfaces for both components, leveraging existing variant types.

---
+++ types/button.ts
@@ -0,0 +1,9 @@
+import { type VariantProps } from "class-variance-authority"
+import { buttonVariants } from "../lib/variants"
+
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
