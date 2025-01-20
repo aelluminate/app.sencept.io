027048033cd735b877e0d5402e4729a7f6ab9810
Noey <ignacio.francis@aelluminate.com>
2025-01-21 01:56:55 +0800

    feat: add shared card and chart components

---
+++ components/shared/chart/_index.ts
@@ -0,0 +1,5 @@
+export { Chart } from "./chart"
+export { ChartTooltip } from "./chart-tooltip"
+export { ChartTooltipContent } from "./chart-tooltip-content"
+export { ChartLegend } from "./chart-legend"
+export { ChartLegendContent } from "./chart-legend-content"
