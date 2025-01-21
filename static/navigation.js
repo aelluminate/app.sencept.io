9659babcb93e2d7bff7d32a222cffdaf50b9d8d6
Noey <ignacio.francis@aelluminate.com>
2025-01-21 16:12:30 +0800

    feat: add static data and navigation configurations

---
+++ static/navigation.js
@@ -0,0 +1,36 @@
+export const navigation = {
+  navMain: [
+    {
+      title: "General",
+      url: "#",
+      items: [
+        {
+          title: "Schemas",
+          url: "#",
+          isActive: true,
+        },
+        {
+          title: "Converters",
+          url: "#",
+          isActive: false,
+        },
+      ],
+    },
+    {
+      title: "Visualizations",
+      url: "#",
+      items: [
+        {
+          title: "Nodes",
+          url: "#",
+          isActive: false,
+        },
+        {
+          title: "Area Charts",
+          url: "#",
+          isActive: false,
+        }
+      ],
+    },
+  ],
+}
