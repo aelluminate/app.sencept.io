191dafe1999829adb45b8895dbb946cda9312c32
Noey <ignacio.francis@aelluminate.com>
2025-01-23 13:13:56 +0800

    (#22) feat: add environment configuration and new constants

--- config/constants.ts
+++ config/constants.ts
@@ -1,3 +1,5 @@
+import "dotenv/config"
+
 /* Sidebars */
 export const SIDEBAR_COOKIE_NAME = "sidebar:state"
 export const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
@@ -8,3 +10,9 @@ export const SIDEBAR_KEYBOARD_SHORTCUT = "b"
 
 /* Themes */
 export const THEMES = { light: "", dark: ".dark" } as const
+
+/** GitHub Link */
+export const GITHUB_REPO_ISSUES = "https://github.com/aelluminate/app.sencept.io/issues"
+
+/** Flask API */
+export const FLASK_API_URL = process.env.NEXT_PUBLIC_FLASK_API_URL
